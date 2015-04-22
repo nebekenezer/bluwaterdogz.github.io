6676cee0ca4b9f38f390d426b93275aa3e2eb989
Bluwaterdogz <bveedy@gmail.com>
2015-04-22 10:30:38 -0700

    Initial Upload

---
+++ js/resume.js
@@ -0,0 +1,58 @@
+	
+	// var $divider1 = $("#divider1");
+	// var $divider2 = $("#divider2");
+	// var $divider3 = $("#divider3");
+	// var $insets = $("#divider3 .bg");
+
+	// var windowHeight = $("window").height;
+
+
+
+
+
+
+
+//$(document).ready(function(){
+//	const SLOWNESS = 10;
+  //   $('section[data-type="background"]').each(function(){
+  //   	var $bgObj = $(this); // assigning the object
+
+		// $(window).scroll(function() {
+		// 	var positionY = -($window.scrollTop() / SLOWNESS);
+		// 	var position = "50% "+positionY +"px" 
+		// 	$bgObj.css({backgroundPosition: position});
+
+	 //    });
+
+ 	// });
+
+
+
+
+
+	// var degrees = 180;
+
+
+	// var rotate = function(degrees){
+	// 	$(this).css({
+	// 		'-webkit-transform' : 'rotate('+ degrees +'deg)',
+ //            '-moz-transform' : 'rotate('+ degrees +'deg)',
+ //            '-ms-transform' : 'rotate('+ degrees +'deg)',
+ //            'transform' : 'rotate('+ degrees +'deg)'
+ //        });
+		
+	// }
+
+	// $("#hexMenu").rotate(30);
+
+
+
+	// $(".hexagon").mousemove(function(e){
+	// 	var mx = e.pageX,
+	// 		my = e.pageY,
+
+
+	// })
+//});
+
+
