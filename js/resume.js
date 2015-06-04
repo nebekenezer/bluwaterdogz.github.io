a83188768d9767c2c4cc91056d13ba7eff3a2efc
Bluwaterdogz <bveedy@gmail.com>
2015-06-04 11:47:28 -0700

    update
    making it look more modern, still working with js

--- js/resume.js
+++ js/resume.js
@@ -1,58 +1,93 @@
-	
-	// var $divider1 = $("#divider1");
-	// var $divider2 = $("#divider2");
-	// var $divider3 = $("#divider3");
-	// var $insets = $("#divider3 .bg");
 
-	// var windowHeight = $("window").height;
+$(document).ready(function(){
 
-
-
-
-
-
-
-//$(document).ready(function(){
-//	const SLOWNESS = 10;
-  //   $('section[data-type="background"]').each(function(){
-  //   	var $bgObj = $(this); // assigning the object
-
-		// $(window).scroll(function() {
-		// 	var positionY = -($window.scrollTop() / SLOWNESS);
-		// 	var position = "50% "+positionY +"px" 
-		// 	$bgObj.css({backgroundPosition: position});
-
-	 //    });
-
- 	// });
-
-
-
-
-
-	// var degrees = 180;
-
-
-	// var rotate = function(degrees){
-	// 	$(this).css({
-	// 		'-webkit-transform' : 'rotate('+ degrees +'deg)',
- //            '-moz-transform' : 'rotate('+ degrees +'deg)',
- //            '-ms-transform' : 'rotate('+ degrees +'deg)',
- //            'transform' : 'rotate('+ degrees +'deg)'
- //        });
+	var speedOffset = 1,
+		xPos = 0,
+		speed900 = 0.8;
 		
-	// }
+		$window = $(window),
 
-	// $("#hexMenu").rotate(30);
+		winW = $window.width(),
+		wimH = $(window).height();
+
+		if(winW < 400){
+			speed900 = 0.001;
+		}
 
 
 
-	// $(".hexagon").mousemove(function(e){
-	// 	var mx = e.pageX,
-	// 		my = e.pageY,
+$('section').each(function(){
 
 
-	// })
-//});
+	var $bgobj = $(this),
+	objPosTop = $bgobj.offset().top,
+	yPos = 0;//$bgobj.position.y;
 
+	
+	$window.resize(function() {
+		var	objPosTop = $bgobj.offset().top,
+			wimH = $(window).height(),
+			winW = $window.width();
+
+		if(winW < 400){
+			speed900 = 0.3;
+		}else{
+			speed900 = 1;
+
+		}
+
+	});
+
+	$window.scroll(function() {
+
+		var objClass = $bgobj.attr("class"),
+		windowPosY = $window.scrollTop(),
+		yPos = 0;
+	console.log($bgobj+" "+yPos);
+		if(
+			windowPosY + $window.height() >=  objPosTop && windowPosY  <= objPosTop + $bgobj.height()
+
+		){
+
+
+			if( objClass == "overlay"  || objClass == "overlayR" ){
+				yPos -= winW/4  * speedOffset;
+			}else{
+				yPos -= winW/12 ;
+			}
+
+			yPos -= ( ( windowPosY - objPosTop ) / $bgobj.attr("data-speed"));
+			
+			var coords = xPos + 'vw '+ yPos + 'px';
+
+			$bgobj.css("background-position", coords);
+		}
+	});
+});
+
+
+$('#divider4').each(function(){
+
+
+			var $bgobj = $(this);
+			var objPosTop = $bgobj.offset().top;
+
+			$window.scroll(function() {
+				var objClass = $bgobj.attr("class");
+				var windowPosY = $window.scrollTop();
+
+				if(
+			windowPosY + $window.height() >=  objPosTop && windowPosY  <= objPosTop + $bgobj.height()
+					){
+					console.log("made it");
+					console.log(windowPosY + $window.height());
+					console.log( objPosTop);
+
+				}
+		});
+	});
+
+
+
+});
 
