3caf7c405b84f2aaeebbf646005e1868e34953e8
Bluwaterdogz <bveedy@gmail.com>
2015-06-05 11:34:27 -0700

    update
    update

--- js/resume.js
+++ js/resume.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
 		$window = $(window),
 
 		winW = $window.width(),
-		wimH = $(window).height();
+		wimH = $window.height();
 
 		if(winW < 400){
-			speed900 = 0.001;
+			speed900 = 1;
 		}
 
 
@@ -22,28 +22,28 @@ $('section').each(function(){
 	var $bgobj = $(this),
 	objPosTop = $bgobj.offset().top,
 	yPos = 0;//$bgobj.position.y;
-
+	console.log($bgobj+" "+yPos);
 	
 	$window.resize(function() {
 		var	objPosTop = $bgobj.offset().top,
 			wimH = $(window).height(),
 			winW = $window.width();
 
-		if(winW < 400){
-			speed900 = 0.3;
-		}else{
-			speed900 = 1;
+		// if(winW < 900){
+		// 	speed900 = 3;
+		// }else{
+		// 	speed900 = 1;
 
-		}
+		// }
 
 	});
 
 	$window.scroll(function() {
 
 		var objClass = $bgobj.attr("class"),
-		windowPosY = $window.scrollTop(),
+		windowPosY = $window.scrollTop();
 		yPos = 0;
-	console.log($bgobj+" "+yPos);
+
 		if(
 			windowPosY + $window.height() >=  objPosTop && windowPosY  <= objPosTop + $bgobj.height()
 
@@ -51,12 +51,12 @@ $('section').each(function(){
 
 
 			if( objClass == "overlay"  || objClass == "overlayR" ){
-				yPos -= winW/4  * speedOffset;
+				yPos -= winW/4  ;//* speedOffset;
 			}else{
 				yPos -= winW/12 ;
 			}
 
-			yPos -= ( ( windowPosY - objPosTop ) / $bgobj.attr("data-speed"));
+			yPos -= ( ( windowPosY - objPosTop ) / $bgobj.attr("data-speed") );
 			
 			var coords = xPos + 'vw '+ yPos + 'px';
 
@@ -64,30 +64,66 @@ $('section').each(function(){
 		}
 	});
 });
+$('h2,table').addClass("scrollfade transparent");
 
 
-$('#divider4').each(function(){
+$('.scrollfade').each(function(){
 
+			var $obj = $(this),
+				objPosTop = $obj.offset().top;
+				objPosBottom = $obj.offset().bottom;
 
-			var $bgobj = $(this);
-			var objPosTop = $bgobj.offset().top;
+			
+			// $window.resize(function (){
+			// 	objPosTop = $obj.offset().top;
+			// 	windowPosY = $window.scrollTop();
+
+			// });
 
 			$window.scroll(function() {
-				var objClass = $bgobj.attr("class");
-				var windowPosY = $window.scrollTop();
+
+				var windowPosY = $window.scrollTop();				
 
 				if(
-			windowPosY + $window.height() >=  objPosTop && windowPosY  <= objPosTop + $bgobj.height()
-					){
-					console.log("made it");
-					console.log(windowPosY + $window.height());
-					console.log( objPosTop);
+					windowPosY + $window.height()/2 >=  objPosTop
 
+					){
+
+					console.log("hit");
+					$obj.removeClass("scrollfade transparent");
 				}
 		});
-	});
+});
 
 
+$(".projects").on("mouseover","li",function(){
+
+
+	var newImg = $(this).index();
+	$(".img-over").addClass("transparent");
+	$(".img-over").eq(newImg).removeClass("transparent");
+});
+
+// $(".projects a").hover(function(){
+// 	var $this = $(this);
+// 	bg = $this.data("pic");
+// 	console.log(bg);
+
+
+// //	$(".viewer").css("background","url("+bg+")");
+
+
+// 	// $(".viewer").animate({
+// 	// 	background: "url("+bg+")"
+// 	// });
+
+
+// });
+
+$('.skill img').on("click", function(){
+
+	$(this).siblings().fadeIn();
 
 });
 
+});
